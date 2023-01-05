2e5b7b8bd9b2a4e2d266dee0d109e6b4fc2fbcfa
NastyonaSavina <nastyona.savina@gmail.com>
2023-01-05 23:12:44 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
+import contactsJson from '../assets/contacts.json';
+
 import styles from './App.module.css';
 import { ContactForm } from '../components/ContactForm/ContactForm';
 import { ContactList } from '../components/ContactList/ContactList';
@@ -13,16 +15,31 @@ export class App extends Component{
 
 
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
 
+    componentDidMount() {
+    console.log('componentDidMount');
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || contactsJson;
+    this.setState({ contacts });
+  }
+
+
+   componentDidUpdate(_, prevState) {
+    if (
+      prevState.contacts.length !== 0 &&
+      prevState.contacts.length !== this.state.contacts.length
+    ) {
+      console.log('componentDidUpdate');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+
+    // this.setState(...);
+   }
+  
   addContact = ({ name, number }) => {
     const Contact = {
       id: nanoid(),
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import styles from '../Filter/Filter.module.css';
 
 
